72201ed5597eef85dc523b8360cce093f14834fc
Fredson Chaves <fredsonchaves07@gmail.com>
2022-02-03 17:53:44 -0300

    feat: add import sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_client (name, cpf, birh_Date, income, children) VALUES ('Francisco', '02745365211', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 45.2, 2);
+INSERT INTO tb_client (name, cpf, birh_Date, income, children) VALUES ('Maria', '02745365211', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 45.2, 2);
+INSERT INTO tb_client (name, cpf, birh_Date, income, children) VALUES ('Carlos', '02745365211', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 45.2, 2);
+INSERT INTO tb_client (name, cpf, birh_Date, income, children) VALUES ('Ana', '02745365211', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 45.2, 2);
+INSERT INTO tb_client (name, cpf, birh_Date, income, children) VALUES ('Francisca', '02745365211', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 45.2, 2);
+INSERT INTO tb_client (name, cpf, birh_Date, income, children) VALUES ('Carol', '02745365211', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 45.2, 2);
+INSERT INTO tb_client (name, cpf, birh_Date, income, children) VALUES ('Jessica', '02745365211', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 45.2, 2);
