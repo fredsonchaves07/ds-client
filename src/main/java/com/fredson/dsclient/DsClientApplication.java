b6de29f701b6fbbea7fe4b9de9acf73cc813d61b
Fredson Chaves <fredsonchaves07@gmail.com>
2022-02-03 17:54:03 -0300

    fix: spring boot application

---
+++ src/main/java/com/fredson/dsclient/DsClientApplication.java
@@ -0,0 +1,12 @@
+package com.fredson.dsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DsClientApplication.class, args);
+    }
+
+}
